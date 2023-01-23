2e1b4bd92dd705d59a994fbe9a21d261a10ff4b0
KSKamburova18 <KSKamburova18@codingburgas.bg>
2023-01-23 12:26:55 +0200

    Initial commit

---
+++ src/main/java/com/example/krasi3/Krasi3Application.java
@@ -0,0 +1,13 @@
+package com.example.krasi3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Krasi3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Krasi3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/krasi3/Krasi3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.krasi3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Krasi3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
